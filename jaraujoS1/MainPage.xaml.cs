15344fae938ff7d87077e3b674fa2ace06a75ca0
Santiago Sevilla <Santiago Sevilla@DESKTOP-C4NPK09>
2023-06-22 14:36:43 -0500

    Agregar archivos de proyecto.

---
+++ jaraujoS1/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace jaraujoS1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSaludo_Clicked(object sender, EventArgs e)
+        {
+            string nombre = txtDato.Text; //
+            int edad = Convert.ToInt32(txtEdad.Text);
+            
+            DisplayAlert("Mensaje de bienvenida", "tienes" + edad, "cerrar");
+        }
+    }
+}
---
+++ jaraujoS1/Registro.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace jaraujoS1
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Registro : ContentPage
+	{
+		public Registro ()
+		{
+			InitializeComponent ();
+		}
+
+        private void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+			Navigation.PushAsync(new MainPage());
+
+        }
+    }
+}
